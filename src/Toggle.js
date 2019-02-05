b62f8a69c316996471a888da11d8189d838b29b2
Mike Lock <michael.lockwitz@consensys.net>
2019-02-04 20:51:09 -0700

    As compound component.

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Switch } from './Switch';
 
 class Toggle extends React.Component {
+  static On = ({ on, children }) => (on ? children : null);
+  static Off = ({ on, children }) => (on ? null : children);
+  static Button = ({ on, toggle, ...props }) => (
+    <Switch on={on} onClick={toggle} {...props} />
+  );
+
   state = {
     on: false,
   };
@@ -16,12 +22,23 @@ class Toggle extends React.Component {
   };
 
   render() {
-    return <Switch on={this.state.on} onClick={this.toggle} />;
+    return React.Children.map(this.props.children, childElement =>
+      React.cloneElement(childElement, {
+        on: this.state.on,
+        toggle: this.toggle,
+      })
+    );
   }
 }
 
 function Usage({ onToggle = (...args) => console.log('onToggle', ...args) }) {
-  return <Toggle onToggle={onToggle} />;
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Button />
+      <Toggle.Off>The button is off</Toggle.Off>
+    </Toggle>
+  );
 }
 
 export default Usage;
