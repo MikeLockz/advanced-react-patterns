cdeb5fe2a1d99f67527b97c44ffdcc2e4e91762c
Mike Lock <michael.lockwitz@consensys.net>
2019-02-04 21:09:10 -0700

    Checking for consumers outside of providers and throw custom error.

--- src/Toggle.js
+++ src/Toggle.js
@@ -3,23 +3,38 @@ import { Switch } from './Switch';
 
 const ToggleContext = React.createContext();
 
+function ToggleConsumer(props) {
+  return (
+    <ToggleContext.Consumer>
+      {context => {
+        if (!context) {
+          throw new Error(
+            'Toggle compond components must be rendered within a Provider component.'
+          );
+        }
+        return props.children(context);
+      }}
+    </ToggleContext.Consumer>
+  );
+}
+
 class Toggle extends React.Component {
   static On = ({ children }) => (
-    <ToggleContext.Consumer>
-      {contextValue => (contextValue.on ? children : null)}
-    </ToggleContext.Consumer>
+    <ToggleConsumer>
+      {({ contextValue }) => (contextValue.on ? children : null)}
+    </ToggleConsumer>
   );
   static Off = ({ children }) => (
-    <ToggleContext.Consumer>
+    <ToggleConsumer>
       {contextValue => (contextValue.on ? null : children)}
-    </ToggleContext.Consumer>
+    </ToggleConsumer>
   );
   static Button = ({ ...props }) => (
-    <ToggleContext.Consumer>
+    <ToggleConsumer>
       {contextValue => (
         <Switch on={contextValue.on} onClick={contextValue.toggle} {...props} />
       )}
-    </ToggleContext.Consumer>
+    </ToggleConsumer>
   );
 
   state = {
@@ -51,13 +66,13 @@ class Toggle extends React.Component {
 
 function Usage({ onToggle = (...args) => console.log('onToggle', ...args) }) {
   return (
-    <Toggle onToggle={onToggle}>
+    <div onToggle={onToggle}>
       <Toggle.On>The button is on</Toggle.On>
       <Toggle.Off>The button is off</Toggle.Off>
       <div>
         <Toggle.Button />
       </div>
-    </Toggle>
+    </div>
   );
 }
 
